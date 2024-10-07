15ba15afe0cd257a30ab10ae1aacd36eb55afb7c
Nomer Maralit <nomerio@Nomers-MacBook-Pro.local>
2024-10-07 23:24:30 +0800

    Change calculator's logic and added error messages

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,29 +1,32 @@
+def valid_number(number):
+    while True:
+        user_input = input(number)
+        try:
+            return float(user_input) # try converting user_input to a float 
+        except ValueError:
+            print('Invalid number, please enter a valid number.') # print if user input an invalid number
+
+# input first number
+num1 = valid_number('Please enter the 1st number: ')
+
+# input operator
 operator = input('Enter an operator (+, -, *, /): ')
-num1 = float(input('Please enter the 1st number: '))
-num2 = float(input('Please enter the 2nd number: '))
+
+# input second number
+num2 = valid_number('Please enter the 2nd number: ')
 
 if operator == '+':
     result = num1 + num2
-    print(round(result, 3))
 elif operator == '-':
     result = num1 - num2
-    print(round(result, 3))
 elif operator == '*':
     result = num1 * num2
-    print(round(result, 3))
 elif operator == '/':
-    result = num1 / num2
-    print(round(result, 3))
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = 'Cannot divide by zero'
 else:
-    print(f'The {operator} is not a valid operator')
-
-
-
-
-
-
-
-
-
-
-
+    result = 'Invalid operator'
+    
+print(f'The result is: {result}')
