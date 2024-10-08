8c79616cdbbed6bd04173a08be970466296b6b50
Nomer Maralit <nomerio@Nomers-MacBook-Pro.local>
2024-10-08 22:26:38 +0800

    Added a functionality to ask user if user wants to perform another calculation.

--- basic_calculator.py
+++ basic_calculator.py
@@ -6,27 +6,39 @@ def valid_number(number):
         except ValueError:
             print('Invalid number, please enter a valid number.') # print if user input an invalid number
 
+while True: #wrap the code in while loop to keep the calculator running
+
 # input first number
-num1 = valid_number('Please enter the 1st number: ')
+    num1 = valid_number('Please enter the 1st number: ')
 
-# input operator
-operator = input('Enter an operator (+, -, *, /): ')
+    # input operator
+    operator = input('Enter an operator (+, -, *, /, ^): ')
 
-# input second number
-num2 = valid_number('Please enter the 2nd number: ')
+    # input second number
+    num2 = valid_number('Please enter the 2nd number: ')
 
-if operator == '+':
-    result = num1 + num2
-elif operator == '-':
-    result = num1 - num2
-elif operator == '*':
-    result = num1 * num2
-elif operator == '/':
-    if num2 != 0:
-        result = num1 / num2
+    # Perform the calculation
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = 'Cannot divide by zero'
+    elif operator == '^':
+        result = num1 ** num2
     else:
-        result = 'Cannot divide by zero'
-else:
-    result = 'Invalid operator'
-    
-print(f'The result is: {result}')
+        result = 'Invalid operator'
+
+    print (num1, operator, num2, '=', result)
+
+    # Ask if user wants to perform another calculation
+    continue_calculation = input('Dp you want to perform another calculation? (y/n): ')
+
+    if continue_calculation.lower() != 'y': #If the user input something other than 'y', exit calculator.
+        print('Exiting calculator...')
+        break
